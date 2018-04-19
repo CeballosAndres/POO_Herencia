366011ad5fe9ca020d168ece2a10b8f9f19b96f5
jacv89 <jacv89@gmail.com>
2018-04-19 15:03:15 -0500

    agregando objetos

--- src/poo_u3p1_herencia/POO_U3P1_Herencia.java
+++ src/poo_u3p1_herencia/POO_U3P1_Herencia.java
@@ -1,16 +1,28 @@
 
 package poo_u3p1_herencia;
 import java.util.Scanner;
+
 public class POO_U3P1_Herencia {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Estudiante est = new Estudiante();
-        System.out.println(est.consultarDatos());
-        System.out.println("RFC del estudiante");
-        est.rfc = sc.next();
         
+        Estudiante est = new Estudiante();
+        Persona per = new Persona();
+        System.out.print("RFC ");
+        per.setRfc(sc.next());
+        System.out.print("Nombre ");
+        per.setNombre(sc.next());
+        System.out.print("Email ");
+        per.setEmail(sc.next());
+        System.out.println(per.getRfc()+", "+per.getNombre()+", "+per.getEmail());
+
+        System.out.println("Datos del estudiante");
         System.out.println(est.consultarDatos());
+
+        Empleado emp = new Empleado();
+        System.out.println("Datos de emplado");
+        System.out.println(emp.consultarDatos());
         
     }
     
