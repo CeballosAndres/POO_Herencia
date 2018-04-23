285dd51af82b0258667e02f0b9304e07d78ab494
jacv89 <jacv89@gmail.com>
2018-04-23 11:35:47 -0500

    lunes

--- src/poo_u3p1_herencia/POO_U3P1_Herencia.java
+++ src/poo_u3p1_herencia/POO_U3P1_Herencia.java
@@ -6,24 +6,30 @@ public class POO_U3P1_Herencia {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
         Estudiante est = new Estudiante();
-        Persona per = new Persona();
-        System.out.print("RFC ");
-        per.setRfc(sc.next());
-        System.out.print("Nombre ");
-        per.setNombre(sc.next());
-        System.out.print("Email ");
-        per.setEmail(sc.next());
-        System.out.println(per.getRfc()+", "+per.getNombre()+", "+per.getEmail());
-
-        System.out.println("Datos del estudiante");
-        System.out.println(est.consultarDatos());
-
-        Empleado emp = new Empleado();
-        System.out.println("Datos de emplado");
-
+        Administrativo adm = new Administrativo();
+        Docente doc = new Docente();
         
+        est.setRfc("JULI-990122");
+        est.setNombre("Juárez López Israel");
+        est.setNoControl("15460132");
+        est.setPromedio(96.3f);
+        
+        adm.setRfc("dasfjñlasdjfl");
+        adm.setNombre("Torres García Alejandro");
+        adm.setClave("201");
+        adm.setSueldo(8429.00f);
+        adm.setPuesto("Cajero");
+        
+        doc.setRfc("ZERO-880812");
+        doc.setNombre("Zepeda Ruíz Oscar");
+        doc.setClave("120");
+        doc.setSueldo(14299.0f);
+        doc.setHoras(20);
+        
+        System.out.println(est.consultarDatos());
+        System.out.println(adm.consultarDatos());
+        System.out.println(doc.consultarDatos());
     }
     
 }
